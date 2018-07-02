614137afb76118922d077a719145083a04947884
Geoff Shannon <geoffpshannon@gmail.com>
2018-07-02 04:24:25 -0700

    Add more tests for cljs-build-task-template

--- test/radicalzephyr/template_test.clj
+++ test/radicalzephyr/template_test.clj
@@ -6,4 +6,26 @@
 (deftest test-thing
   (is (= '(deftask build []
             (cljs))
-         (sut/cljs-build-task-template [] []))))
+         (sut/cljs-build-task-template [] [])))
+
+  (is (= '(deftask build []
+            (task1)
+            (task2)
+            (cljs))
+         (sut/cljs-build-task-template '[(task1) (task2)]
+                                       [])))
+
+  (is (= '(deftask build []
+            (cljs)
+            (after1)
+            (after2))
+         (sut/cljs-build-task-template []
+                                       '[(after1)
+                                         (after2)])))
+
+  (is (= '(deftask build []
+            (before1)
+            (cljs)
+            (after1))
+         (sut/cljs-build-task-template '[(before1)]
+                                       '[(after1)]))))
